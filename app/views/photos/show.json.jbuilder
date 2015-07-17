fc683bf55ad47271151cdf3cdc2a215de34a43eb
Wanning <wanning@yahoo-inc.com>
2015-07-16 23:40:52 -0700

    update login/signup ui

---
+++ app/views/photos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @photo, :id, :photographer, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716062726) do
+ActiveRecord::Schema.define(version: 20150717060255) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "photographer"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
